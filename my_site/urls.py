f0c7a9243f3f9d5827db62d933a0e363002216df
Gourav Kumar Singh <gourav.19b131001@abes.ac.in>
2021-11-19 20:52:10 +0530

    Some changes

--- my_site/urls.py
+++ my_site/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^files/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-] 
+]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
 #   + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
