4c5bdc08537c58fdf5950525461ef35d05f04cfe
Gourav Kumar Singh <gourav.19b131001@abes.ac.in>
2021-11-19 16:00:12 +0530

    static changes

--- my_site/settings.py
+++ my_site/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
